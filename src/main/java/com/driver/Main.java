67839079113edc3483ab2e15a3efcb06fa36a62c
Pranjal Agarwal <pranjalagarwal6@gmail.com>
2022-12-17 16:48:42 +0530

    Demo commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
+class A{
+    public String meth(){
+        return "Invoking method from class A";
+    }
+}
+class B extends A{
+
+}
 public class Main {
-  
+  B obj = new B();
+  String str = obj.meth();
 }
